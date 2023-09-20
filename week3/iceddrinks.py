162e0891c2b8c28fea4862294329077127cca51c
danho <danho011185@gmail.com>
2023-09-20 22:54:46 +0900

    icedrinks

--- week3/iceddrinks.py
+++ week3/iceddrinks.py
@@ -1,32 +1,76 @@
+import sys
+
+# input=sys.stdin.readline
+
+# 상 하 좌 우
+dx=[0, 0, -1, 1]
+dy=[-1, 1, 0, 0]
 
 
-def dfs(x,y):
-    if x<0 or x>=n or y<0 or y>=m:
-        return False
-    if graph[x][y]==0: 
-        graph[x][y]=1
-        dfs(x,y-1) # 상
-        dfs(x,y+1) # 하
-        dfs(x-1,y) # 좌
-        dfs(x+1,y) # 우
-        return True
-    
-    
-    return False
-
-
-n,m=map(int, input().split())
-
-graph=[]
-
-for i in range(n):
-    graph.append(list(map(int,input())))
-
-
-count=0
-for i in range(n):
-    for j in range(m):
-        if dfs(i,j):
+def solution(graph, n,m):
+    count=0
+    for i in range(m):
+        for j in range(n):
+            if graph[j][i]==1:
+                continue
+            
             count+=1
+            graph[j][i]=1
+            dfs(graph, i, j, n, m)
 
-print(count)
+            
+            # print(graph)
+
+    return count
+
+def dfs(graph, x,y,n,m):
+
+    
+    
+    for i in range(4):
+        # 상 하 좌 우 
+        tmp_y=y+dy[i]
+        tmp_x=x+dx[i]
+
+        if tmp_y<0 or tmp_y >=n or tmp_x < 0 or tmp_x >=m:
+            continue
+        else:
+            wall=graph[tmp_y][tmp_x]
+            if wall==0:
+                graph[tmp_y][tmp_x]=1
+                dfs(graph, tmp_x, tmp_y, n, m)
+
+    
+
+
+    
+
+# input 
+n, m=map(int, input().split())
+
+
+# ex
+graph=[
+    [0,0,0,0,0,1,1,1,1,0,0,0,0,0],
+    [1,1,1,1,1,1,0,1,1,1,1,1,1,0],
+    [1,1,0,1,1,1,0,1,1,0,1,1,1,0],
+    [1,1,0,1,1,1,0,1,1,0,0,0,0,0],
+    [1,1,0,1,1,1,1,1,1,1,1,1,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,1,0,0],
+    [1,1,0,0,0,0,0,0,0,1,1,1,1,1],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    [0,0,0,0,0,0,0,0,0,1,1,1,1,1],
+    [0,1,1,1,1,1,1,1,1,1,1,0,0,0],
+    [0,0,0,1,1,1,1,1,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,1,1,1,1,0,0,0],
+    [1,1,1,1,1,1,1,1,1,1,0,0,1,1],
+    [1,1,1,0,0,0,1,1,1,1,1,1,1,1],
+    [1,1,1,0,0,0,1,1,1,1,1,1,1,1]
+]
+# graph=[]
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+
+
+anw=solution(graph, n, m)
+print(anw)

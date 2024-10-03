4f86b05e33054bc025def91915299e7103887550
danho <82808719+jjpterdh@users.noreply.github.com>
2024-10-03 21:58:19 +0900

    [level 2] Title: 배달, Time: 4.69 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,46 +1,66 @@
 import java.util.*;
+class Node implements Comparable<Node> {
+    int idx;
+    int cost;
+    Node(int idx, int cost) {
+        this.idx=idx;
+        this.cost=cost;
+    }
+    
+    @Override
+    public int compareTo(Node o) {
+        return this.cost-o.cost;
+    }
+}
 
 class Solution {
-    int[][] board;
+    ArrayList<ArrayList<Node>> graph;
+    int[] dist;
     public int solution(int N, int[][] road, int K) {
         int answer = 0;
-
-        board= new int[N+1][N+1];
-        for(int i=1;i<=N;i++) {
-            Arrays.fill(board[i], 500001);
+        graph=new ArrayList<ArrayList<Node>>();
+        
+        for(int i=0;i<=N;i++) {
+            graph.add(new ArrayList<Node>());
         }
         
-        // init
-        for(int[] arr: road) {
+        for(int[] arr : road) {
             int start=arr[0];
-            int end=arr[1];
-            int cost=arr[2];
-            board[start][end]=Math.min(board[start][end], cost);
-           board[end][start]=Math.min(board[end][start], cost);
+            int end= arr[1];
+            int cost= arr[2];
+            graph.get(start).add(new Node(end, cost));
+            graph.get(end).add(new Node(start, cost));
         }
         
-        // 플로이드-와샬
-        for(int k=1;k<=N;k++) {
-            for(int start=1;start<=N;start++) {
-                
-                if(start==k) {
-                    board[start][k]=0;
-                    continue;
-                }
-                for(int end =1; end<=N; end++) {
-                    if(end==k) continue;
-                    board[start][end]=Math.min(board[start][end], board[start][k]+board[k][end]);
-                    board[end][start]=Math.min(board[end][start], board[end][k]+board[k][start]);
-                }
-            }
+        
+        dist=new int[N+1];
+        dijkstra(1, N);
+        
+        for(int i=1;i<=N;i++) {
+            if(dist[i]<=K) answer++;
         }
         
-        for(int i=1;i<=N;i++){
-            if(board[1][i]<=K) {
-                // System.out.println("start : "+ 1 + " end : "+i + " cost : "+K);
-                answer++;
-            }
-        }
         return answer;
+    }
+    public void dijkstra(int start, int N) {
+        PriorityQueue<Node> pq= new PriorityQueue<Node>();
+        Arrays.fill(dist, 99999999);
+        pq.offer(new Node(start, 0));
+        dist[start]=0;
+        
+        while(!pq.isEmpty()) {
+            Node cur= pq.poll();
+            if(dist[cur.idx]< cur.cost) continue;
+            
+            for(Node next : graph.get(cur.idx)) {
+                int new_dist= dist[cur.idx]+next.cost;
+                if(dist[next.idx] >= new_dist) {
+                    dist[next.idx]=new_dist;
+                    pq.offer(new Node(next.idx, new_dist));
+                }
+            }
+        }
+        
+        
     }
 }

670b419915c309bd242cd3d7a1265c791b3ec577
danho <82808719+jjpterdh@users.noreply.github.com>
2024-11-14 03:24:45 +0900

    [D3] Title: XY 문자열 1, Time: 209 ms, Memory: 65,784 KB -BaekjoonHub

--- SWEA/D3/20955. XY 문자열 1/XY 문자열 1.java
+++ SWEA/D3/20955. XY 문자열 1/XY 문자열 1.java
@@ -4,34 +4,34 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Solution
-{
- 
-    public static void main(String [] args )throws IOException {
+public class Solution {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-         
-        int T = Integer.parseInt(br.readLine());
-         
-        for(int i = 1; i <= T; i++) {
-            String S = br.readLine();
-            String E = br.readLine();
-             
-            String answer = canTransform(S, E) ? "Yes" : "No";
-            sb.append("#").append(i).append(" ").append(answer).append("\n");
-        }
-         
-        System.out.println(sb.toString());
-    }
-     
-    public static boolean canTransform(String S, String E) {
-        while (E.length() > S.length()) {
-            if (E.endsWith("X")) {
-                E = E.substring(0, E.length() - 1);
-            } else if (E.endsWith("Y")) {
-                E = new StringBuilder(E.substring(0, E.length() - 1)).reverse().toString();
+
+
+        int T=Integer.parseInt(br.readLine());
+        for (int i = 1; i <= T; i++) {
+            String S=br.readLine();
+            String E =br.readLine();
+            boolean flag=false;
+
+            while(E.length()>S.length()) {
+
+                if(E.endsWith("X")) {
+                    E=E.substring(0, E.length()-1);
+                }
+                else if(E.endsWith("Y")) {
+                    E=E.substring(0, E.length()-1);
+                    E = new StringBuilder(E).reverse().toString();
+                }
+
+//                System.out.println(E);
+
             }
+
+
+            if(E.equals(S)) System.out.println("#"+i+" Yes");
+            else System.out.println("#"+i+" No");
         }
-        return E.equals(S);
     }
 }
